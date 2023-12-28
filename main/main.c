d0ba314775ce134bd3e84622e5244c6223737396
vinicius.lemos <vinicius.lemos@inatel.br>
2023-12-28 11:37:17 -0300

    [main, sdkconfig] codigo base confluence

--- main/main.c
+++ main/main.c
@@ -5,23 +5,22 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/FreeRTOSConfig.h"
 #include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_log.h"
 #include "esp_task_wdt.h"
-#include "esp_sleep.h"
 
 #define TAG "[MAIN]"
-#define SEC_TO_US 1000000
 
 void foo_task(void *pvParameters)
 {
-    // Enables WDT monitorament on this task
+    // Habilita o monitoramento nessa task
     esp_task_wdt_add(NULL);
  
     while(true)
     {
-        // Feeds the WDT each 1 sec
+        // Realimenta o WDT
         esp_task_wdt_reset();
 
         vTaskDelay(pdMS_TO_TICKS(1000));
@@ -29,38 +28,34 @@ void foo_task(void *pvParameters)
     }
 }
 
-void start_deep_sleep(uint16_t sleep_seconds)
-{
-    esp_sleep_enable_timer_wakeup(sleep_seconds * SEC_TO_US);
-    esp_deep_sleep_start();
-}
-
 void app_main(void)
 {
-    esp_err_t err = 0;
+    esp_err_t err;
     esp_task_wdt_config_t wdt_config={0};
 
-    // Sets the timeout to 2 sec
-    wdt_config.timeout_ms = 2000;
-    wdt_config.trigger_panic = false;
+    // Seta o timeout para 3 segundos
+    wdt_config.timeout_ms = 3000;
+    // Monitorando idle tasks de todos os nucleos
+    wdt_config.idle_core_mask = (1 << portNUM_PROCESSORS) - 1;
+    // Gatilho do panic ativado
+    wdt_config.trigger_panic = true; 
 
+    // Verifica se TWDT ainda nao foi inicializado
+#if !CONFIG_ESP_TASK_WDT_INIT
     err = esp_task_wdt_init(&wdt_config);
-    if(err == ESP_ERR_INVALID_STATE) // Already initialized
+    ESP_LOGI(TAG, "Initialization %s", (err == ESP_OK)?"success":"fail");
+#else
+    // Se ja foi inicializado, apenas reconfigura
+    err = esp_task_wdt_reconfigure(&wdt_config);
+    ESP_LOGI(TAG, "Reconfigure %s", (err == ESP_OK)?"success":"fail");
+#endif
+    // Verifica se foi resetado pelo TWDT
+    if(esp_reset_reason() == ESP_RST_TASK_WDT)
     {
-        err = esp_task_wdt_reconfigure(&wdt_config);
-        ESP_LOGW(TAG, "Reconfigure %s", (err == ESP_OK)?"success":"fail");
-    }
-    if(esp_reset_reason() == ESP_RST_DEEPSLEEP)
-    {
-        ESP_LOGI(TAG, "Wake up from Deep-Sleep");
-        vTaskDelay(pdMS_TO_TICKS(5000));
+        ESP_LOGI(TAG, "Reset caused by Task WDT");
     }
 
-    // Create task
+    // Inicializa a Task
     ESP_LOGI(TAG, "Foo task created");
-    xTaskCreate(foo_task, "Foo task", 2048, NULL, 1, NULL);
-
-    // Works normally for 10 seconds
-    vTaskDelay(pdMS_TO_TICKS(10000));
-    // start_deep_sleep(10);
+    xTaskCreate(foo_task, "Foo task", 2048, NULL, 1, NULL); 
 }
